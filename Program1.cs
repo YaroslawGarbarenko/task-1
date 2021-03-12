12c02ed42d399b6aaa48f2d1d72e1f2376e20a1c
YaroslawGarbarenko <72256923+YaroslawGarbarenko@users.noreply.github.com>
2021-03-12 08:28:25 +0200

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class RIN
+    {
+        int min = 0;    
+        int max = 100;  
+        public string info = "число"; 
+        public bool n_bool; 
+
+        
+        int n;
+        public int N   
+        {
+            get { return n; }
+            set
+            {
+                if (value < min)
+                {
+                    n = min;
+                    n_bool = false;
+                }
+                else if (value > max)
+                {
+                    n = max;
+                    n_bool = false;
+                }
+                else
+                {
+                    n = value;
+                    n_bool = true;
+                }
+            }
+        }
+        public RIN(string n_st, int n_min, int n_max, int n_def, string n_info)
+        {
+            min = n_min;
+            max = n_max;
+            info = n_info;
+            n_bool = true;
+            try
+            {
+                N = Convert.ToInt32(n_st);
+                if (!n_bool)
+                {
+                    info = "Ошибка ввода параметра << " + info + " >>. Число вне диапазона. Автоматически присваивается нижняя/верхняя граница. Для изменения введите целое число от " + min.ToString() + " до " + max.ToString();
+                }
+            }
+            catch
+            {
+                info = "Ошибка ввода параметра << " + info + " >>. Введите целое число от " + min.ToString() + " до " + max.ToString() + ". По умолчанию параметр =  " + n_def.ToString(); ;
+                n = n_def;
+                n_bool = false;
+            }
+        }
+    }   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+
+            Console.WriteLine("Введите число а");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число b");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число c");
+            c = Convert.ToInt32(Console.ReadLine());
+            d = (a + b + c) / 3;
+            Console.WriteLine( "среднее арифметическое =" + d);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp1._2
+{
+    class Program
+    {
+        delegate int Add(int x, int y);
+        delegate int Sub(int x, int y);
+        delegate int Mul(int x, int y);
+        delegate int Div(int x, int y);
+        static void Main(string[] args)
+        {
+            int a, b;
+            // Add
+            Console.WriteLine("Введите число (а) для оператора Add");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число (b) для оператора Add");
+            b = Convert.ToInt32(Console.ReadLine());
+            Add add = (x, y) => x + y;
+            Console.WriteLine(add(a,b) + " - Оператор Add-добавдения");
+            int c, d;
+            // Sub
+            Console.WriteLine("Введите число (c) для оператора Sub");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число (b) для оператора Sub");
+            d = Convert.ToInt32(Console.ReadLine());
+            Sub sub = (x, y) => x - y;
+            Console.WriteLine(sub(c, d) + " - Оператор Sub-вычетания");
+            int e, f;
+            // Mul
+            Console.WriteLine("Введите число (e) для оператора Mul");
+            e = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число (f) для оператора Mul");
+            f = Convert.ToInt32(Console.ReadLine());
+            Mul mul = (x, y) => x * y;
+            Console.WriteLine(mul(e, f) + " - Оператор Mul-умножения");
+            int g, i;
+            // Div
+            Console.WriteLine("Введите число (g) для оператора Div");
+            g = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число (i) для оператора Div");
+            i = Convert.ToInt32(Console.ReadLine());
+            Div div = (x, y) => x / y;
+            Console.WriteLine(mul(g, i) + " - Оператор Div-деления");
+              try
+              {
+                Console.WriteLine(g / i);
+              }
+              catch (DivideByZeroException)
+              {
+                Console.WriteLine("Division of {0} by zero.", g);
+              }
+
+            Console.ReadLine();
+        }
+        
+    }
+}
